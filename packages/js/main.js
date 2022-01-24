8b54e7a50c0c3812d01018216974b42635ee1e77
Rotaract Club of IIT <89523696+RACIIT@users.noreply.github.com>
2022-01-24 20:25:59 +0530

    Update main.js

--- packages/js/main.js
+++ packages/js/main.js
@@ -69,7 +69,7 @@ window.addEventListener("load", () => {
   let totalSeconds;
 
   function init() {
-    totalSeconds = Math.floor((new Date("01/22/2022") - new Date()) / 1000);
+    totalSeconds = Math.floor((new Date("01/28/2022") - new Date()) / 1000);
     setTimeLeft();
     let interval = setInterval(() => {
       if (totalSeconds < 0) {
